e0ecf23c58e9c96e170be4c4b5ebb9cad861af1f
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-26 15:54:58 -0300

    Adicionando algumas configurações no eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,35 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // Aqui você pode adicionar outras regras personalizadas ou ajustar as existentes
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
