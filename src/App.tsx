1bcd9d3e62de2e05a1452dfc755e0a899cf00615
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-26 15:59:35 -0300

    Renderizando os componentes de card e formulario de um novo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import "./App.css";
+import { Card } from './components/card/Card';
+import { useFoodData } from './hooks/useFoodData';
+import { FormFood } from './components/card/Form/FormFood';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Cardápio</h1>
+      <div className="card-grid">
+        {data?.map(foodData => 
+          <Card 
+            key={foodData.id}
+            price={foodData.price} 
+            title={foodData.title} 
+            image={foodData.image}
+            />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {isModalOpen && <FormFood closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal} >Novo Comida</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
